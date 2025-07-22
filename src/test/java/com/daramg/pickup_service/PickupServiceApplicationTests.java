c3e10ac7104ee225cd85e6dd6cde8d70a736b38e
양효빈 <hyobin3026@naver.com>
2025-07-22 19:34:26 +0900

    [CHORE] 프로젝트 초기설정

---
+++ src/test/java/com/daramg/pickup_service/PickupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daramg.pickup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
